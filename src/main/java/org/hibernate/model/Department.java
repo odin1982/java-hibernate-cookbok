7ba1f7d176b411e01d655a74d45c3e3754d2294d
odinPcWin10 <odin.araujo.barragan@gmail.com>
2020-12-14 18:50:50 -0600

    Providing an XML-based hibernate mapping

---
+++ src/main/java/org/hibernate/model/Department.java
@@ -0,0 +1,26 @@
+package org.hibernate.model;
+
+public class Department {
+	private Long id;
+	private String deptName;
+	
+	public Department() {}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDeptName() {
+		return deptName;
+	}
+
+	public void setDeptName(String deptName) {
+		this.deptName = deptName;
+	}
+	
+	
+}
---
+++ src/main/java/org/hibernate/model/Employee.java
@@ -0,0 +1,44 @@
+package org.hibernate.model;
+
+public class Employee {
+	private Long id;
+	private String firstName;
+	private Double salary;
+	private Department department;
+	
+	public Employee() {}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public Double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(Double salary) {
+		this.salary = salary;
+	}
+
+	public Department getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(Department department) {
+		this.department = department;
+	}
+	
+	
+}
