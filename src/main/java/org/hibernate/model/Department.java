38e57048d89576155f8b19f68276621b13025bcf
odinPcWin10 <odin.araujo.barragan@gmail.com>
2020-12-14 19:44:47 -0600

    Mapping with XML

--- src/main/java/org/hibernate/model/Department.java
+++ src/main/java/org/hibernate/model/Department.java
@@ -1,6 +1,14 @@
 package org.hibernate.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 public class Department {
+	
 	private Long id;
 	private String deptName;
 	
--- src/main/java/org/hibernate/model/Employee.java
+++ src/main/java/org/hibernate/model/Employee.java
@@ -1,6 +1,17 @@
 package org.hibernate.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+
 public class Employee {
+
 	private Long id;
 	private String firstName;
 	private Double salary;
