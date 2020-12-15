9c6f73d49cb917a51e0d9c2210ca9493720cb91c
odinPcWin10 <odin.araujo.barragan@gmail.com>
2020-12-15 07:03:19 -0600

    Configuracion para varias bases de datos

--- src/main/java/org/hibernate/config/BuildingSessionFactory.java
+++ src/main/java/org/hibernate/config/BuildingSessionFactory.java
@@ -5,8 +5,12 @@ import org.hibernate.cfg.Configuration;
 
 public class BuildingSessionFactory {
 	public static void main(String[] args) {
-		Configuration cfg = new Configuration();					//Creates blank configuration
-		cfg = cfg.configure();										//look for hibernate.cfg.xml or hibernate.properties
-		SessionFactory sessionFactory = cfg.buildSessionFactory();	// create Session Factory
+		Configuration cfg = new Configuration();					// Creates blank configuration
+		//option 1: without cfg.xml file
+		//cfg = cfg.configure();										// Look for hibernate.cfg.xml or hibernate.properties if you dont set file configuration
+		
+		//option 2: with cfg.xml file
+		//cfg = cfg.configure("postgresql.cfg.xml");					// You specify postgresql
+		SessionFactory sessionFactory = cfg.buildSessionFactory();	// Create Session Factory
 	}
 }
